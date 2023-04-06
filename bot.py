e0b0adf68ed5975da5c1880741973b6e4765715e
Natasha <nnkonovalova@bk.ru>
2023-04-06 14:56:32 +0300

    created class WeatherApi

--- bot.py
+++ bot.py
@@ -1,16 +1,20 @@
 
-
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-
-#https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid={c1187b3eaa7f19a879772ca85bc06244}&lang=ru&units=metric
-
+import urllib.parse
+import urllib.request
+import json
 
 class WeatherApi:
     def __init__(self, token):
-        self.tooken = token
+        self.token = token
 
+    def data_for(self, *args):
+        query = urllib.parse.quote_plus(','.join(args))
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={query}&appid={self.token}&lang=ru&units=metric"
+        response = urllib.request.urlopen(url)
+        data = json.loads(response.read().decode())
+        return data
 
 
 class Bot:
@@ -47,8 +51,13 @@ class Bot:
         self.dispatcher.add_handler(unknown_handler)
 
     def show_weather(self, update, context):
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                  text="")
+        data = self.weather_api.data_for(*context.args)
+        weather_data = f"{data['weather'][0]['description'].capitalize()}"
+        temp_data = f"\nТемпература: {data['main']['temp']}. Ощущается как {data['main']['feels_like']}"
+        wind_data = f"\nСкорость ветра: {data['wind']['speed']} м/с"
+        context.bot.send_message(chat_id=update.effective_chat.id, 
+                                 text=f"{weather_data}{temp_data}{wind_data}")
+
 
     def start(self, update, context):
         context.bot.send_message(chat_id=update.effective_chat.id,
